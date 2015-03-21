72ad447d60ee7ced6f2a1d61776cb37b379a5e01
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-21 02:06:13 -0500

    added basis for js

--- html/index.php
+++ html/index.php
@@ -11,11 +11,13 @@
     </h3>
     <form method="post">
         <input type="text" id="title" maxlength="80">
-            THIS IS WHERE THE STORY GOES
+            THIS IS WHERE THE TITLE GOES
         </input>
         <input type="text" id="story" maxlength="40000">
             THIS IS WHERE THE STORY GOES
         </input>
         <input type="submit"></input>
     </form>
+    <script src="js/jquery.js"></script>
+    <script src="js/logic.js"></script>
 </body>
--- html/post.php
+++ html/post.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=localhost dbname=storytiem user=postgres password=")
+$conn = pg_connect("host=localhost dbname=storytiem user=ras password='john madden'")
     or die ('is dead ' . pg_last_error());
 $query = 'select * from posts';
 $result = pg_query($query) or die('is dead ' . pg_last_error());
