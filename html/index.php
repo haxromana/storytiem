cd2a1a0114bdba6ec921983d0e2d2d8a11907980
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-03-21 16:25:16 -0500

    Added client-size verification for input size

--- html/index.php
+++ html/index.php
@@ -40,6 +40,38 @@
     $(document).ready(function () {
         fixTextareaWidth();
         $("h5").text(getHooks());
+        errorMessage = "Please match the requested format.";
+
+    $( this ).find( "textarea" ).on( "input change propertychange", function() {
+
+        var pattern = $( this ).attr( "pattern" );
+
+        if(typeof pattern !== typeof undefined && pattern !== false)
+        {
+            var patternRegex = new RegExp( "^" + pattern.replace("^\^|\$$") + "$", "g" );
+
+            hasError = !$( this ).val().match( patternRegex );
+
+            if ( typeof this.setCustomValidity === "function") 
+            {
+                this.setCustomValidity( hasError ? errorMessage : "" );
+            } 
+            else 
+            {
+                $( this ).toggleClass( "error", !!hasError );
+                $( this ).toggleClass( "ok", !hasError );
+
+                if ( hasError ) 
+                {
+                    $( this ).attr( "title", errorMessage );
+                } 
+                else
+                {
+                    $( this ).removeAttr( "title" );
+                }
+            }
+        }
+
     });
     
 </script>
