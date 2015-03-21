8425b8f495f9d50c64074fa1b7541848339bc4ce
Ben Williams <ben.metaman72@gmail.com>
2015-03-21 02:31:05 -0500

    Added css formatting for index.php

--- html/index.php
+++ html/index.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf8">
     <title>storytiem!</title>
-    <link rel="stylesheet" type="text/css" href="style.css">        
+    <link rel="stylesheet" type="text/css" href="css/style.css">        
 </head>
 
 <body>
