d50a49ab8beec9203f741d0fc5255ffbd1723e59
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-03-21 13:33:52 -0500

    Changed array to not brokeded

--- html/js/logic.js
+++ html/js/logic.js
@@ -18,7 +18,7 @@ function showStory() {
 }
 
 function getHooks() {
-    var hooks = {"Collective stories about the human condition",
+    var hooks = ["Collective stories about the human condition",
                      "Speak friend and enter...",
                      "After nourishment, shelter and companionship, stories are the thing we need most in the world.",
                      "The more you leave out, the more you highlight what you leave in.",
@@ -28,7 +28,7 @@ function getHooks() {
                      "By telling stories, you objectify your own experience. You separate it from yourself. You pin down certain truths. You make up others.",
                      "Call me Ishmael...",
                      "It was the best of times. It was the worst of times.",
-                     "This here's a story all about how my life got flipped, turned upside down."}
+                     "This here's a story all about how my life got flipped, turned upside down."]
     return hooks[Math.floor(Math.random()*(hooks.length+1))];
 }
 
