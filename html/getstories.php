602d539df2ce1c62c75067b01d2e86dbaebe9ceb
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-21 16:43:29 -0500

    more hiding

--- html/getstories.php
+++ html/getstories.php
@@ -6,7 +6,7 @@
     for ($i = 0; $i < pg_num_rows($result); $i++) {
         pg_result_seek($result, $i);
         $row = pg_fetch_assoc($result);
-        echo "<div class='story' style='display:inline-block' id='" . $row['id'] . "'>\n";
+        echo "<div class='story' style='display:none' id='" . $row['id'] . "'>\n";
         echo "<div class='title'>" . nl2br(htmlentities($row['title'])) ."\n<br></div>\n" 
             . "<div class='date'>posted at " . date("j F, Y", strtotime($row['time'])) . "<br></div>" 
             . "<div class='post'>" . nl2br(htmlentities($row['post'])) . "<br></div>";
