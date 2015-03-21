0275a62211576ce46586e985b58b4c320b9de307
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-21 12:30:18 -0500

    did time things

--- html/getstories.php
+++ html/getstories.php
@@ -1,13 +1,15 @@
 <html>
 <?php  
     $conn = pg_connect("host=localhost dbname=storytiem user=ras password='john madden'");
-    $sql = 'select * from posts order by time desc;';
+    $sql = 'select * from posts order by time desc limit 30;';
     $result = pg_query($sql);
     for ($i = 0; $i < pg_num_rows($result); $i++) {
         pg_result_seek($result, $i);
         $row = pg_fetch_assoc($result);
         echo "<div class='story' style='display:block' id='" . $row['id'] . "'>\n";
-        echo nl2br(htmlentities($row['title'])) ."\n<br>\n" . nl2br(htmlentities($row['post'])) . "<br>";
+        echo "<div class='title'>" . nl2br(htmlentities($row['title'])) ."\n<br></div>\n" 
+            . "<div class='date'>posted at " . date("j F, Y", strtotime($row['time'])) . "<br></div>" 
+            . "<div class='post'>" . nl2br(htmlentities($row['post'])) . "<br></div>";
         if ($row['id'] == $_COOKIE["auth"]) {
             echo "(you)";
         }
