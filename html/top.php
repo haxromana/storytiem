40d923907459cd365c2f30491ec6de9a2362352d
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-21 16:51:32 -0500

    added different kinds of rankings

---
+++ html/top.php
@@ -0,0 +1,30 @@
+<!doctype HTML>
+<html>
+<head>
+    <meta charset="utf8">
+    <title>storytiem!</title>
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>                   
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <h3>
+        Stories
+    </h3>
+
+    <div id='stories'>
+
+    </div>
+
+    <div class="grayUpvote"></div>
+    <div class="grayDownvote"></div>
+</body>
+<script src='js/jquery.js'></script>
+<script src='js/logic.js'></script>
+<script src='ajax.js'></script>
+<script>    
+$(document).ready(function () {
+    getPostsTop();
+});
+</script>
+</html>
