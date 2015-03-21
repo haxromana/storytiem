059a99376a9f13bb343bc9922cc73bd21dae68e9
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-21 06:30:23 -0500

    thank you based torvalds

--- html/post.php
+++ html/post.php
@@ -1,12 +1,11 @@
 <?php
 	$conn = pg_connect("host=localhost dbname=storytiem user=ras password='john madden'");
-    echo "connected";
+    
     if ($_POST) {
-        echo 'post data received';
-        print_r($_POST);
+        
         extract($_POST);
         $id = hash("md5", $story . date());
-        
+        setcookie("auth", $id, time()+3600);
 		$sql = "insert into posts(post, title, score, id) values ('$story', '$title', 0, '$id')";
         $result = pg_query($conn, $sql);
         if ($result == FALSE) {
--- html/story.php
+++ html/story.php
@@ -11,7 +11,21 @@
         Stories
     </h3>
 
-<div id='stories'></div>
+<div id='stories'>
+<?php
+    $_COOKIE["auth"];
+    $conn = pg_connect("host=localhost dbname=storytiem user=ras password='john madden'");
+    $sql = 'select * from posts order by time desc;';
+    $result = pg_query($sql);
+    for ($i = 0; $i < pg_num_rows($result); $i++) {
+        pg_result_seek($result, $i);
+        $row = pg_fetch_assoc($result);
+        echo "<div class='story' style='display:block' id='" . $row['id'] . "'>\n";
+        echo "<div class='title'>" . $row['title'] ."</div>\n<br>\n<div class='post'>".$row['post'] . "</div><br>";
+        echo "</div>\n";
+    }
+?>
+</div>
 
 </body>
 <script src='js/jquery.js'></script>
