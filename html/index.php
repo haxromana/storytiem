e8c094c4f5a8147120c18bf6cb7739f99ab51e8d
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-03-21 05:42:44 -0500

    WTF is going on idk mang

--- html/index.php
+++ html/index.php
@@ -3,19 +3,22 @@
 <head>
     <meta charset="utf8">
     <title>storytiem!</title>
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="css/style.css">        
 </head>
 
 <body>
     <h3>
-        Storeeeeee
+        Storee
     </h3>
     <form method="post" action="post.php" enctype="multipart/formdata">
-        THIS IS WHERE THE TITLE GOES
-        <input type="text" name="title" id="inputTitle" maxlength="80" placeholder="THIS IS WHERE THE TITLE GOES"></input>
-        THIS IS WHERE THE STORY GOES
-        <input type="textarea" name="story" id="inputStory" maxlength="40000" placeholder="THIS IS WHERE THE STORY GOES"></input>
-        
+        <input type="text" name="title" id="inputTitle" maxlength="80"
+            placeholder="Enter a title for your story here"></input>
+        <div class="expandingArea">
+            <pre><span></span></pre>
+            <input type="textarea" name="story" id="inputStory" maxlength="40000"
+                placeholder="Enter your story here"></input>
+        </div>
         <input type="submit" id="button" value="submit"></input>
     </form>
     <form method="link" action="story.php">
