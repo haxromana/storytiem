a418df3781301a731315146c5373425bc80c7ec9
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-21 18:35:27 -0500

    changed and fixed validation

--- html/index.php
+++ html/index.php
@@ -5,6 +5,21 @@
     <title>storytiem!</title>
     <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="css/style.css">        
+    <script>
+    function validate() {
+        var title = $("#inputTitle");
+        var txtarea = $("#inputStory");
+        if (title.val().length < 4) {
+            alert('title too short');
+            return false;
+        }
+        if (txtarea.val().length < 144) {
+            alert('post too short');
+            return false;
+        }
+        return true;
+    }
+    </script>
 </head>
 
 <body>
@@ -15,12 +30,12 @@
         
     </h5>
     <div id='post_form'>
-    <form method="post" action="post.php" enctype="multipart/formdata">
+    <form method="post" action="post.php" onsubmit="return validate()" enctype="multipart/formdata">
         <input type="text" name="title" id="inputTitle" maxlength="80"
-            pattern='.{3,}' required title='3 characters minimum'
+            required title='3 characters minimum'
             placeholder="Enter a title for your story here"></input>
         <textarea name="story" id="inputStory" wrap='on' rows='4'
-            pattern='.{500,}' required title='500 characters minimum'
+            required title='500 characters minimum'
             placeholder="Enter your story here"></textarea>
         <input type="submit" class="button" value="submit"></input>
     </form>
@@ -38,36 +53,8 @@
         $("h5").text(getHooks());
         errorMessage = "Please match the requested format.";
 
-    $( this ).find( "textarea" ).on( "input change propertychange", function() {
+    
+});
 
-        var pattern = $( this ).attr( "pattern" );
-
-        if(typeof pattern !== typeof undefined && pattern !== false)
-        {
-            var patternRegex = new RegExp( "^" + pattern.replace("^\^|\$$") + "$", "g" );
-
-            hasError = !$( this ).val().match( patternRegex );
-
-            if ( typeof this.setCustomValidity === "function") 
-            {
-                this.setCustomValidity( hasError ? errorMessage : "" );
-            } 
-            else 
-            {
-                $( this ).toggleClass( "error", !!hasError );
-                $( this ).toggleClass( "ok", !hasError );
-
-                if ( hasError ) 
-                {
-                    $( this ).attr( "title", errorMessage );
-                } 
-                else
-                {
-                    $( this ).removeAttr( "title" );
-                }
-            }
-        }
-
-    });});
     
 </script>
