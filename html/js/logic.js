42a7d1cd1a224b57f4b7b0135e04c743cdd81e4b
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-03-21 21:06:58 -0500

    updated the quotes

--- html/js/logic.js
+++ html/js/logic.js
@@ -57,12 +57,13 @@ function getHooks() {
                      "After nourishment, shelter and companionship, stories are the thing we need most in the world.",
                      "The more you leave out, the more you highlight what you leave in.",
                      "Of course it is happening inside your head, Harry, but why on earth should that mean that it is not real?",
-                     "John Madden, John Madden, John Madden",
                      "Artists use lies to tell the truth. Yes, I created a lie. But because you believed it, you found something true about yourself.",
                      "By telling stories, you objectify your own experience. You separate it from yourself. You pin down certain truths. You make up others.",
                      "Call me Ishmael...",
                      "It was the best of times. It was the worst of times.",
                      "This here's a story all about how my life got flipped, turned upside down.",
-                     "brbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbrbr"]
+                     "There's so much grey to every story - nothing is so black and white.",
+                     "Stay a while and listen.",
+                     "There is no greater agony than bearing an untold story inside you."]
     return hooks[Math.floor(Math.random()*(hooks.length))];
 }
